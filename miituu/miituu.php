6677bfeeb08fb627fbaee3c6365651e9e8197cb3
Malcolm Christie <malcolm@sbp-creative.com>
2013-08-27 14:49:17 +0100

    Bleu

---
+++ miituu/miituu.php
@@ -0,0 +1,38 @@
+<?php
+
+use Guzzle\Http\Client;
+namespace miituu;
+
+class miituu
+{
+    private $base = 'http://api.miituu.com/';
+
+    public function __construct()
+    {
+
+    }
+
+    public static function startFromSlug($slug)
+    {
+
+    }
+
+    public function call($endpoint, $data = array(), $method = 'GET', $auth = true)
+    {
+        $client = new Client(self::$base);
+
+        $headers = ($auth && self::$token) ? array('headers/X-API-Token' => self::$token) : array();
+
+        if ($method == 'GET') {
+            $request = $client->get($endpoint, $headers, array(
+                'query' => $data
+            ));
+
+        } else if ($method == 'POST') {
+            $request = $client->get($endpoint, $headers, $data);
+
+        }
+
+        dd($request);
+    }
+}
